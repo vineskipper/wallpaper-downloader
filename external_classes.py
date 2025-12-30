2f90aef6fa4a7954e438ba86e4fd492fd15a6dd4
Vineskipper <divine.ezinma@gmail.com>
2025-12-30 17:09:23 -0600

    updated LinkSourceList and Link classes

--- external_classes.py
+++ external_classes.py
@@ -3,12 +3,15 @@ class Link:
         return Link(arr[0], arr[1], arr[2])
 
     def __init__(self, link: str, newName: str, targetDirectoryPath: str):
-        self.link: str = link
-        self.fileName: str = newName
-        self.targetDirectoryPath: str = targetDirectoryPath
+        self._link: str = link
+        self._fileName: str = newName
+        self._targetDirectoryPath: str = targetDirectoryPath
 
     def __str__(self):
-        return self.link
+        return self._link
+    
+    def download(self):
+        ...
 
 class LinkSourceList:
     def __init__ (self, filePath: str):
@@ -33,10 +36,24 @@ class LinkSourceList:
                     line = line.split(" , ")
                     self._mailList[currentDir].append(Link(line[0], line[1], currentDir))
 
+        self._links = []
+
+        for arr in self.getMailList().values():
+            for link in arr:
+                self._links.append(link)
+
     def getMailList(self) -> dict:
         return self._mailList
+    
+    def getLinks(self) -> tuple:
+        return self._links
 
-    def printLinks(self):
-        for k, arr in self.getMailList().items():
-            for v in arr:
-                print(f"{k}: {v}")
+    def printLinks(self, showDirectories: bool = False):
+        if showDirectories:
+            for k, arr in self.getMailList().items():
+                for v in arr:
+                    print(f"{k}: {v}")
+        else:
+            for link in self.getLinks():
+                print(link)
+
