50df59511c507db6930f8b94d020eb094404f41a
Vineskipper <divine.ezinma@gmail.com>
2025-12-30 20:05:04 -0600

    updated printLinks function, outlined download function for links, added a imported a few libraries to be used later

--- external_classes.py
+++ external_classes.py
@@ -1,3 +1,7 @@
+import tqdm # for progess bars
+import requests # for downloading files with error handling capacities
+import os # for files handling
+
 class Link:
     def tupleToLink(arr: tuple) -> Link:
         return Link(arr[0], arr[1], arr[2])
@@ -10,13 +14,21 @@ class Link:
     def __str__(self):
         return self._link
     
-    def download(self):
-        ...
+    def download(self): # return a status code on the success of the download process
+        response = requests.get(self._link)
+
+        if response.status_code == 200:
+            ...
+        
+        else:
+            #FIXME: error handling
+            ...
 
 class LinkSourceList:
-    def __init__ (self, filePath: str):
+    def __init__ (self, filePath: str, parentDirectoryPath: str, format=None):
         self._filePath = filePath
         self._mailList: dict =  {}
+        self._parentDirectoryPath = os.path.abspath(parentDirectoryPath)
 
         with open(self._filePath) as src:
             while True:
@@ -34,7 +46,7 @@ class LinkSourceList:
                     self._mailList[currentDir] = []
                 else:
                     line = line.split(" , ")
-                    self._mailList[currentDir].append(Link(line[0], line[1], currentDir))
+                    self._mailList[currentDir].append(Link(line[0], line[1], os.path.join(self._parentDirectoryPath, currentDir)))
 
         self._links = []
 
@@ -45,15 +57,16 @@ class LinkSourceList:
     def getMailList(self) -> dict:
         return self._mailList
     
-    def getLinks(self) -> tuple:
+    def getLinks(self) -> tuple[Link]:
         return self._links
 
-    def printLinks(self, showDirectories: bool = False):
-        if showDirectories:
-            for k, arr in self.getMailList().items():
-                for v in arr:
-                    print(f"{k}: {v}")
+    def printLinks(self, verbose: bool = False):
+        if verbose:
+            for link in self.getLinks():
+                print(f"({link._link}) goes to [{link._targetDirectoryPath}] as \"{link._fileName}\"")
         else:
             for link in self.getLinks():
                 print(link)
 
+    def downloadAll(self):
+        ...
