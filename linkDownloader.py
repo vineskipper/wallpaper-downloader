b5308bf779dc1b737d2286325edd5b75b032ece0
Vineskipper <divine.ezinma@gmail.com>
2025-12-31 18:46:11 -0600

    outlined furture features

--- linkDownloader.py
+++ linkDownloader.py
@@ -3,14 +3,21 @@ from time import sleep # for a delaying
 import os # for file checking and command execution
 import external_classes as ec # for Link and LinkSourceList classes
 
+'''TODO: future additions
+- add format option numbers for snakecase and camelcase, automatic file naming, and automatically adding ook extension
+- add an interactive argument with image previews and editable filenames where you can manually choose which files to be downloaded
+- add a silent argument where errors are ignored and not displayed
+'''
+
 def clearScreen(waitTime = 0):
     sleep(waitTime) and os.system("cls") if os.name == "nt" else os.system("clear")
 
 parser = argparse.ArgumentParser(prog="linkDownloader.py", description="Downloads and stores files using links from a text file")
 parser.add_argument("filePath", help="file to read from", metavar="file path", nargs="?", default=None)
 parser.add_argument("parentDirectory", help="parent directory to store downloaded files in", metavar="parent directory", nargs="?", default=None)
-parser.add_argument("-s", "--formatStyle", help="formatting style for directory names", metavar="optionNumber") #FIXME: add option numbers for snakecase and camelcase
-
+parser.add_argument("-f", "--formatStyle", help="formatting style for directory names", metavar="optionNumber")
+parser.add_argument("-s", "--silent", action="store_true", help="enable silent mode")
+parser.add_argument("-i", "--interactive", action="store_true", help="enable interactive mode")
 #TODO: future arguments go here
 
 args = parser.parse_args()
@@ -31,4 +38,9 @@ clearScreen()
 
 srcList = ec.LinkSourceList(FILEPATH, PARENT_DIRECTORY)
 
-srcList.printLinks(showDirectories=True)
+srcList.printLinks(verbose=True)
+
+# testing
+
+input(">> ")
+srcList.downloadAll()
